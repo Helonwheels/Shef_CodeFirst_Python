d2957f257751908ebe24ba5dffcacdea7de07ca7
Unknown <hftheaker1@sheffield.ac.uk>
2017-10-25 18:44:28 +0100

    Add collaborative

--- session1/collaborative.py
+++ session1/collaborative.py
@@ -1,2 +1,6 @@
 # Hi there welcome to the collaborative exercise!
 # Start typing your answers below
+age= 40
+hobby = "playing music"
+Description = "My age is {} and I like {}".format (age, hobby)
+print (Description)
